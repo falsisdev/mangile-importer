88cfbe7556fd4ba4d09f46ffb10553087c6ff232
falsisdev <keremtokul@hotmail.com>
2026-07-05 19:54:45 +0300

    feat: add page reader for CBZ archive

--- internal/importer/archive.go
+++ internal/importer/archive.go
@@ -1,6 +1,10 @@
 package importer
 
-import "archive/zip"
+import (
+	"archive/zip"
+	"fmt"
+	"io"
+)
 
 type Archive struct {
 	reader *zip.ReadCloser
@@ -19,4 +23,14 @@ func Open(path string) (*Archive, error) {
 
 func (a *Archive) Close() error {
 	return a.reader.Close()
+}
+
+func (a *Archive) OpenPage(path string) (io.ReadCloser, error) {
+	for _, file := range a.reader.File {
+		if file.Name == path {
+			return file.Open()
+		}
+	}
+
+	return nil, fmt.Errorf("page not found: %s", path)
 }
